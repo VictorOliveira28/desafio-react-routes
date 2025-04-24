ff20b42882929f0017ae477343b5ea715a6d29fa
Victor Oliveira <victor.oliveira20@icloud.com>
2025-04-24 14:10:08 -0300

    Install React Router DOM and create page header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import Header from "./components/Header";
+import "./index.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import "./styles.css";
+import imgHome from "../../../assets/home.svg";
+
+export default function Header() {
+  return (
+    <>
+      <header className="hd-container">
+        <nav className="nav-hd-container">
+          <div className="nav-menu-container">
+            <div>Início</div>
+            <div>Produtos</div>
+            <div>Sobre nós</div>
+          </div>
+          <div className="nav-home-button">
+            <img src={imgHome} alt="Página inicial" />
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
